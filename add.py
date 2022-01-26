9d0adb738b86d12363b88f346035cc028b85b663
sanjay <sanjay.kumar@314ecorp.com>
2022-01-26 22:28:28 +0530

    modified version of add method

--- add.py
+++ add.py
@@ -1,5 +1,8 @@
-def add(a,b):
-    return(a + b)
-print(add(4,5))
-# This is single line comment in python
+# adding the new type of method for adding the two numbers
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+# calling the add method 
+print(add(98,98))
 
